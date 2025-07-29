3b7318e64c0ed308468a58f9d7d047966be5a164
ProgramN128 <gamekimg360@gmail.com>
2025-07-29 11:20:29 +0400

    Initial commit

---
+++ src/main/java/com/sabakachabaka/miscellaneousmod/MiscellaneousMod.java
@@ -0,0 +1,10 @@
+package com.sabakachabaka.miscellaneousmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("miscellaneousmod")
+public class MiscellaneousMod {
+    public MiscellaneousMod() {
+
+    }
+}

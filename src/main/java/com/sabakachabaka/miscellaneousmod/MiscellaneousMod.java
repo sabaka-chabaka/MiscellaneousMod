63c26c0f77efb96472827221cd9cd566218f8e60
ProgramN128 <gamekimg360@gmail.com>
2025-07-29 11:34:11 +0400

    Commit

--- src/main/java/com/sabakachabaka/miscellaneousmod/MiscellaneousMod.java
+++ src/main/java/com/sabakachabaka/miscellaneousmod/MiscellaneousMod.java
@@ -4,6 +4,8 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod("miscellaneousmod")
 public class MiscellaneousMod {
+    public static final String MOD_ID = "miscellaneousmod";
+
     public MiscellaneousMod() {
 
     }
